6ac99f38b1d58a0d6c8f765a00a8696bf529a0df
WillDreamer <1685301782@qq.com>
2024-12-05 01:11:16 -0800

    util update

--- utilities.py
+++ utilities.py
@@ -173,3 +173,76 @@ class LpLoss(object):
 
     def __call__(self, x, y):
         return self.rel(x, y)
+
+
+class RandomMeshGenerator(object):
+    def __init__(self, real_space, mesh_size, sample_size):
+        super(RandomMeshGenerator, self).__init__()
+
+        self.d = len(real_space)
+        self.m = sample_size
+
+        assert len(mesh_size) == self.d
+
+        if self.d == 1:
+            self.n = mesh_size[0]
+            self.grid = np.linspace(real_space[0][0], real_space[0][1], self.n).reshape((self.n, 1))
+        else:
+            self.n = 1
+            grids = []
+            for j in range(self.d):
+                grids.append(np.linspace(real_space[j][0], real_space[j][1], mesh_size[j]))
+                self.n *= mesh_size[j]
+
+            self.grid = np.vstack([xx.ravel() for xx in np.meshgrid(*grids)]).T
+
+        if self.m > self.n:
+                self.m = self.n
+
+        self.idx = np.array(range(self.n))
+        self.grid_sample = self.grid
+
+
+    def sample(self):
+        perm = torch.randperm(self.n)
+        self.idx = perm[:self.m]
+        self.grid_sample = self.grid[self.idx]
+        return self.idx
+
+    def get_grid(self):
+        return torch.tensor(self.grid_sample, dtype=torch.float)
+
+    def ball_connectivity(self, r):
+        pwd = sklearn.metrics.pairwise_distances(self.grid_sample)
+        self.edge_index = np.vstack(np.where(pwd <= r))
+        self.n_edges = self.edge_index.shape[1]
+
+        return torch.tensor(self.edge_index, dtype=torch.long)
+
+    def gaussian_connectivity(self, sigma):
+        pwd = sklearn.metrics.pairwise_distances(self.grid_sample)
+        rbf = np.exp(-pwd**2/sigma**2)
+        sample = np.random.binomial(1,rbf)
+        self.edge_index = np.vstack(np.where(sample))
+        self.n_edges = self.edge_index.shape[1]
+        return torch.tensor(self.edge_index, dtype=torch.long)
+
+    def attributes(self, f=None, theta=None):
+        if f is None:
+            if theta is None:
+                edge_attr = self.grid[self.edge_index.T].reshape((self.n_edges, -1))
+            else:
+                theta = theta[self.idx]
+                edge_attr = np.zeros((self.n_edges, 3 * self.d))
+                edge_attr[:, 0:2 * self.d] = self.grid_sample[self.edge_index.T].reshape((self.n_edges, -1))
+                edge_attr[:, 2 * self.d] = theta[self.edge_index[0]]
+                edge_attr[:, 2 * self.d + 1] = theta[self.edge_index[1]]
+        else:
+            xy = self.grid_sample[self.edge_index.T].reshape((self.n_edges, -1))
+            if theta is None:
+                edge_attr = f(xy[:, 0:self.d], xy[:, self.d:])
+            else:
+                theta = theta[self.idx]
+                edge_attr = f(xy[:, 0:self.d], xy[:, self.d:], theta[self.edge_index[0]], theta[self.edge_index[1]])
+
+        return torch.tensor(edge_attr, dtype=torch.float)
